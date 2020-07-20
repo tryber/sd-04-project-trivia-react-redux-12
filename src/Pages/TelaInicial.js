15eeacb5268e982193f379ddba4f49340907653b
Victor L <victor.lustosacp@gmail.com>
2020-07-20 16:45:34 -0300

    CC

--- src/Pages/TelaInicial.js
+++ src/Pages/TelaInicial.js
@@ -9,8 +9,8 @@ export class TelaInicial extends Component {
     };
     this.verificaçao = this.verificaçao.bind(this);
   }
-  verificaçao(email, name) {
-    if (!email || !name) return true;
+  verificaçao() {
+    if (!this.state.email || !this.state.name) return true;
     return false;
   }
   render() {
@@ -19,25 +19,19 @@ export class TelaInicial extends Component {
       <div>
         <form>
           <label htmlFor="email">Email do Gravatar</label>
-          <input
-            type="text"
-            id="email"
+          <input type="text" id="email"
             data-testid="input-gravatar-email"
             value={email}
             onChange={(event) => this.setState({ email: event.target.value })}
           />
           <label htmlFor="name">Nome do Jogador</label>
-          <input
-            type="text"
-            id="name"
+          <input type="text" id="name"
             data-testid="input-player-name"
             value={name}
             onChange={(event) => this.setState({ name: event.target.value })}
           />
-          <button
-            type="submit"
-            id="Jogar"
-            disabled={this.verificaçao(email, name)}
+          <button type="submit" id="Jogar"
+            disabled={this.verificaçao()}
             data-testid="btn-play"
           >
             Jogar
