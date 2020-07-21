7d2764a453aa7b2f4bfa859926e4bbde9c9c4cf8
douglaza <douglaza@gmail.com>
2020-07-21 17:38:46 -0300

    feedback card t3

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,24 +4,20 @@ import bat from '../bat.png';
 import logo from '../trivia.png';
 
 export default class Feedback extends Component {
-  testeRender = () => {
-    alert('wat');
-  }
-
   render() {
     return (
       <div className="CardFeedback">
         <div className="HeaderFeedback">
-          <img className="LogoSmall" src={logo} alt="logo" />     
+          <img className="LogoSmall" src={logo} alt="logo" />
           <div className="HeaderFeedBackGroup">
             <div className="HeaderFeedbackPic">
               <img data-testid="header-profile-picture" src={bat} alt="gravatar" />
-            </div>          
-            <div className="HeaderFeebackInfo">          
+            </div>
+            <div className="HeaderFeebackInfo">
               <p data-testid="header-player-name">Fullano de Tall</p>
               <p data-testid="header-score">Current score</p>
             </div>
-          </div>          
+          </div>
         </div>
         <div className="BodyFeedback">
           <p data-testid="feedback-text">Mensagem de feedback</p>
@@ -29,9 +25,9 @@ export default class Feedback extends Component {
           <p data-testid="feedback-total-question">Hits</p>
         </div>
         <div className="FooterFeedback">
-          <Button data-testid="btn-play-again" variant="contained" onClick={this.testeRender} color="primary">Jogar Novamente</Button>
-          <Button data-testid="btn-ranking" variant="contained" onClick={this.testeRender} color="primary">Ver Ranking</Button>
-        </div>  
+          <Button data-testid="btn-play-again" variant="contained" color="primary">Jogar Novamente</Button>
+          <Button data-testid="btn-ranking" variant="contained" color="primary">Ver Ranking</Button>
+        </div>
       </div>
     );
   }
