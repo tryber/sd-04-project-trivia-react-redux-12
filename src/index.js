276efd65a85db8c520b9fabea4891109f93fc39c
Thiago Tavares <thiagooo15@gmail.com>
2020-07-20 21:41:08 -0300

    cc fiz lef 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/index';
