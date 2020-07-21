4fd33e379d49107507ddbd52f1209110d226d6e3
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-21 17:42:36 -0300

    Inserted missing "data-testid" attributes

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -15,21 +15,22 @@ export default class Ranking extends Component {
           <h1>RANKING</h1>
         </div>
         <ul>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
-          <li>Ciclano</li>
-          <li>FULANO</li>
+          <li>
+            <span data-testid="player-name-0">Ciclano: </span>
+            <span data-testid="player-score-0">100</span>
+          </li>
+          <li>
+            <span data-testid="player-name-1">Ciclano: </span>
+            <span data-testid="player-score-1">100</span>
+          </li>
+          <li>
+            <span data-testid="player-name-2">Ciclano: </span>
+            <span data-testid="player-score-2">100</span>
+          </li>
+          <li>
+            <span data-testid="player-name-2">Ciclano: </span>
+            <span data-testid="player-score-2">100</span>
+          </li>
         </ul>
         <Button
           type="button"
