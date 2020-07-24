9a4307be2ba6cde54bea77a9ca44c540ee4ffb04
Victor L <victor.lustosacp@gmail.com>
2020-07-24 14:47:10 -0300

    Melhorando o codigo

---
+++ src/components/OpçoesRespostas.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import questions from '../mock_data/questions';
+
+class OpçoesRespostas extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      correct: '',
+      wrong: '',
+      random:'false',
+    };
+    this.shuffle = this.shuffle.bind(this);
+  }
+
+  handleClick1 = () => this.setState({correct:'Correct', wrong:'Wrong', random:'true'});
+
+  shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  }
+
+  render() {
+    const { contador } = this.props;
+    const { correct, wrong, random } = this.state;
+    const objQuestions = questions.results[contador];
+    const arrayResposts = [];
+      arrayResposts.push(...objQuestions.incorrect_answers);
+      arrayResposts.push(objQuestions.correct_answer);
+      if( random === false) this.shuffle(arrayResposts);
+    return (
+      <div>
+        {arrayResposts.map((element, index) => (
+            element === objQuestions.correct_answer ? (
+              <button
+                key={element} data-testid={'correct-answer'}
+                className={correct} onClick={() => this.handleClick1()}
+              >
+                {element}
+              </button>
+            ) : (
+              <button
+                key={element} data-testid={`wrong-answer-${index}`}
+                className={wrong} onClick={() => this.handleClick1()}
+              >
+                {element}
+              </button>
+            )))}
+      </div>
+    )
+  }
+}
+
+export default OpçoesRespostas;
--- src/components/Perguntas.js
+++ src/components/Perguntas.js
@@ -1,80 +1,36 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import questions from '../mock_data/questions';
-import shuffle from 'knuth-shuffle';
+import OpçoesRespostas from './OpçoesRespostas';
 
 class Perguntas extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      numero: 0,
-      classe1:'teste2',
-      classe2:'teste2',
+      contador: 0
     };
-    this.shuffle = this.shuffle.bind(this);
   }
 
-  shuffle(array) {
-    var m = array.length, t, i;
-    // While there remain elements to shuffle…
-    while (m) {
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
-    }
-  
-    return array;
-  }
-
-  handleClick1 = () => this.setState({classe1:'teste', classe2:'teste3'});
-
-  handleClick2 = (numero) => this.setState({ numero: numero + 1 });
+  handleClick2 = (contador) => this.setState({ contador: contador + 1 });
 
   render() {
-    const { numero, classe1, classe2 } = this.state;
-    if (questions.results.length - 1 >= numero) {
-      const arrayResposts = [];
-      arrayResposts.push(...questions.results[numero].incorrect_answers);
-      arrayResposts.push(questions.results[numero].correct_answer);
-      console.log(arrayResposts);
-      this.shuffle(arrayResposts);
-      console.log(arrayResposts);
+    const { contador } = this.state;
+    const objQuestions = questions.results[contador];
       return (
         <div>
-          <p data-testid="question-category">{questions.results[numero].category}</p>
-          <p data-testid="question-text">{questions.results[numero].question}</p>
-          {arrayResposts.map((element, index) => (
-            element === questions.results[numero].correct_answer ? (
-              <button
-                key={element} data-testid={'correct-answer'}
-                className={classe1}
-                onClick={() => this.handleClick1()}
-              >
-                {element}
-              </button>
-            ) : (
-              <button
-                key={element}
-                data-testid={`wrong-answer-${index}`}
-                className={classe2}
-                onClick={() => this.handleClick1()}
-              >
-                {element}
-              </button>
-            )))}
-          <Button onClick={() => this.handleClick2(numero)} data-testid="btn-next">
-            confirmar
+          <p data-testid="question-category">{objQuestions.category}</p>
+          <p data-testid="question-text">{objQuestions.question}</p>
+          <OpçoesRespostas contador={contador} />
+          { contador === questions.results.length - 1 ? (
+          <Link to="/feedback" data-testid="btn-next">feedback</Link>
+        ) : (
+          <Button onClick={() => this.handleClick2(contador)} data-testid="btn-next">
+              confirmar
           </Button>
+        )}
         </div>
       );
-    } return (
-      <div>
-        <p>fim</p>
-      </div>
-    );
   }
 }
 
