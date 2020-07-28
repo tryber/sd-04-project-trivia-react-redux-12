ccda17196d5ce634e7e296cfe00874eae9720351
Thiago Tavares <thiagooo15@gmail.com>
2020-07-28 05:59:57 -0300

    almost give up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
 import md5 from 'crypto-js/md5';
-import PropTypes from 'prop-types';
 import logo from '../trivia.png';
 
-function Header(props) {
-  const { email, name, score } = props;
-  return (
-    <header className="HeaderCard">
-      <div className="HeaderFeedback">
-        <img className="LogoSmall" src={logo} alt="logo" />
-        <div className="HeaderFeedBackGroup">
-          <div className="HeaderFeedbackPic">
-            <img data-testid="header-profile-picture" src={`https://www.gravatar.com/avatar/${md5(email).toString()}`} alt="gravatar" />
-          </div>
-          <div className="HeaderFeebackInfo">
-            <p data-testid="header-player-name">
-Jogador:
-              {name}
-            </p>
-            <p data-testid="header-score">
-Pontuação:
-              {score}
-            </p>
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      assertions: 0,
+      name: '',
+      email: '',
+    };
+  }
+
+  render() {
+    const { name, email, score } = this.state;
+    return (
+      <header className="HeaderCard">
+        <div className="HeaderFeedback">
+          <img className="LogoSmall" src={logo} alt="logo" />
+          <div className="HeaderFeedBackGroup">
+            <div className="HeaderFeedbackPic">
+              <img
+                data-testid="header-profile-picture"
+                src={`https://www.gravatar.com/avatar/${md5(email).toString()}`}
+                alt="gravatar"
+              />
+            </div>
+            <div className="HeaderFeebackInfo">
+              <p data-testid="header-player-name">
+            Jogador:
+                {` ${name}`}
+              </p>
+              <p data-testid="header-score">
+            Pontuação:
+                {` ${score}`}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  }
 }
-
-Header.propTypes = {
-  email: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  score: PropTypes.number.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  email: state.players.email,
-  name: state.players.name,
-  score: state.players.score,
-});
-
-export default connect(mapStateToProps)(Header);
